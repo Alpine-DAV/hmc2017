75ad24ba08cdf1f05e16ca45eb93b06d967b8bb7
Brian Gallagher <gallagher23@llnl.gov>
2017-09-12 13:55:40 -0700

    Added readAllCyclesForFailedZone function.

--- src/DataReader/read_failure.py
+++ src/DataReader/read_failure.py
@@ -12,20 +12,11 @@ np.set_printoptions(suppress=True, precision=4)
 reader = FeatureDataReader(sys.argv[1])
 fails = reader.getAllFailures()
 
-meta = reader.readMetaData(0)
-numFeats = len(meta['features'])
-
 for i in range(len(fails)):
     (run,cycle,zone,vals) = fails[i]
-
-    with open('%s/timeSeries/failure_r%04d_z%06d.npy' % 
-              (sys.argv[1],run,zone), 'rb') as fin:
-        # assumes contiguous cycles starting at 0
-        # failure cycle is the last cycle of that time series
-        data = np.fromfile(fin, dtype=np.float32, count=(cycle+1)*numFeats)
-        data = np.reshape(data, (cycle+1,numFeats))
+    data = reader.readAllCyclesForFailedZone(run,cycle,zone)
         
-        print 'Failure-%d:' % i
-        print np.asarray(vals)
-        print data[cycle]
+    print 'Failure-%d:' % i
+    print np.asarray(vals)
+    print data[cycle]
         
