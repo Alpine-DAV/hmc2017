32115fa340990244a18686ef17c04682f5618c4d
saltedsky <a.huang.011@gmail.com>
2017-11-06 19:05:43 -0800

    added nbmpi

--- src/learning_wrapper.py
+++ src/learning_wrapper.py
@@ -15,14 +15,15 @@ import os
 import random
 import scipy
 from scipy.spatial.distance import euclidean
-from sklearn.naive_bayes import GaussianNB
+# from sklearn.naive_bayes import GaussianNB
 # from sklearn.ensemble.forest import RandomForestRegressor
 import sys
 import time
 import testing_naivebayes as bayes
 import testing_randomforest as rand_forest
 import nbmpi
-from sklearn import datasets
+import rfmpi
+from utils import *
 
 comm = MPI.COMM_WORLD
 
@@ -89,19 +90,6 @@ def get_num_partitions(data_dir):
 
     files = glob.glob("%s/indexes/indexes_p*_r000.txt" % data_dir)
     return len(files)
-
-#
-# Get features names in order they appear in feature vectors
-#
-feature_name_cache = None
-def get_feature_names(data_dir):
-  global feature_name_cache
-  if feature_name_cache is None:
-    reader = get_reader(data_dir)
-    feature_name_cache = reader.getFeatureNames()
-
-  return feature_name_cache
-
 
 #
 # Returns a pair (index,data) where:
@@ -305,8 +293,6 @@ def get_learning_data_for_run(data_dir, start_cycle, end_cycle, sample_freq, dec
     # combine X and Y
     XY = np.hstack((X,Y))
 
-    np.savetxt("FeatureData.csv", XY, delimiter=',')
-
     # debug output
 
     # cache data for subsequent calls to this function
@@ -357,64 +343,98 @@ def add_features(index):
 
 #####################################################################
 
-def wrapper(ML_type, k, data_path, verbose=False, use_online=False):
+def wrapper(ML_type, k, data_path, verbose=False, use_online=False, use_mpi=False):
     """ input: type of machine learning, type of test, amount to test, training path, test path
         output: trains ML_type on training data and tests it on testing data
     """
 
-    dataset = get_data(data_path, TEST_ON_TRAIN_SPEC)
-    X = dataset[:,0:-1]
-    y = np.ravel(dataset[:,[-1]])
-    train, test = data_split(data_path, 1.0/k, TEST_ON_TRAIN_SPEC)
+    # ## only read bubbleShock on rank 0
+    # if comm.rank == 0:
+    #     bubbleShock = get_bubbleshock()
+    #     X = bubbleShock[:,0:-1]
+    #     y = np.ravel(bubbleShock[:,[-1]])
+
+    #     discretized_y = np.zeros(len(y))
+    #     for element in range(len(y)):
+    #         if y[element] != 0:
+    #             discretized_y[element] = 1
+
+    #     # delete shapeAndSize, shearAndSize
+    #     # X = np.delete(X, np.s_[9,11], 1)
+    # else:
+    #     X = np.empty(shape=(96520, 16), dtype=np.float64)
+    #     y = np.empty(96520, dtype=np.float64)
+    #     discretized_y = np.empty(len(y))
+
+    # X = comm.Bcast([X, MPI.DOUBLE])
+    # y = comm.Bcast([y, MPI.DOUBLE])
+    # discretized_y = comm.Bcast([discretized_y, MPI.DOUBLE])
+
+
+    # giving up on not reading bubbleShock n times
+    bubbleShock = get_bubbleshock()
+    X = bubbleShock[:,0:-1]
+    y = np.ravel(bubbleShock[:,[-1]])
 
     discretized_y = np.zeros(len(y))
     for element in range(len(y)):
         if y[element] != 0:
             discretized_y[element] = 1
 
-    # X = np.delete(X, np.s_[0,1,2,3,4,5], 1)
 
     if ML_type == "naive bayes":
-        print "############ Training using Naive Bayes ############"
-        # bayes.train_many_test_many(train, test, TEST_ON_TRAIN_SPEC)
-        y = discretized_y
-
-        bayes.train_and_test_k_fold(X, y, k)
-
-        print 
-        print
+        if comm.rank == 0:
+            print "############ Training using Naive Bayes ############"
+            y = discretized_y
+            result = bayes.train_and_test_k_fold(X, y, k)
+            print "PERFORMANCE\t%s" % (result,)
+            print 
+            print
     elif ML_type == "random forest":
-        print "############ Training using Random Forest ############"
-        rand_forest.train_and_test_k_fold(X, y, k)
-        print 
-        print
+        if comm.rank == 0:
+            print "############ Training using Random Forest ############"
+            result = rand_forest.train_and_test_k_fold(X, y, k)
+            print "PERFORMANCE\t%s" % (result,) 
+            print 
+            print
     elif ML_type == "nbmpi":
-        print "############ Training using Parallel Naive Bayes ############"
-        y = discretized_y
+        if comm.rank == 0:
+            print "############ Training using Parallel Naive Bayes ############"
 
-        use_mpi = 'MPICH_INTERFACE_HOSTNAME' in os.environ
+        y = discretized_y
 
         if use_mpi and comm.rank == 0:
             print('will train using MPI')
         if use_online and comm.rank == 0:
             print('will train in online mode')
 
-        fp, fn = nbmpi.train_and_test_k_fold(X, y, k=k, verbose=verbose, use_online=use_online, use_mpi=use_mpi)
-
+        result = train_and_test_k_fold(X, y, nbmpi.train, k=k, verbose=verbose, use_online=use_online, use_mpi=use_mpi)
+        
         if comm.rank == 0:
-            print "PERFORMANCE\t%d\t%d" % (fp, fn)
+            print "PERFORMANCE\t%s" % (result,)
+
+    elif ML_type == "rfmpi":
+        if comm.rank == 0:
+            print "############ Training using Parallel Random Forest ############"
+
+        if use_mpi and comm.rank == 0:
+            print('will train using MPI')
+        if use_online and comm.rank == 0:
+            print('will train in online mode')
+
+        result = train_and_test_k_fold(X, y, rfmpi.train, k=k, verbose=verbose, use_online=use_online, use_mpi=use_mpi)
+        if comm.rank == 0:
+            print "PERFORMANCE\t%s" % (result,)
+
 
     else:
         raise Exception('Machine learning algorithm not recognized')
 
 def data_split(data_path, k, test_data_spec):
     all_data = get_bubbleshock()
-
-    # all_data = np.delete(all_data, np.s_[0,1,2,3,4,6,8,9,10,11,12,13,14,15], 1)
     k_percent = int(len(all_data)*k)
     train = all_data[:k_percent,:]
     test = all_data[k_percent:,:]
-
     return train, test
 
 def get_bubbleshock():
@@ -436,6 +456,7 @@ if __name__ == '__main__':
     parser.add_argument('-nb', action='store_true', help='train using naive bayes')
     parser.add_argument('-rf', action='store_true', help='train using random forest')
     parser.add_argument('-nbp', action='store_true', help='train using parallel naive bayes')
+    parser.add_argument('-rfp', action='store_true', help='train using parallel naive bayes')
     args = parser.parse_args()
 
     verbose    = args.verbose
@@ -443,33 +464,16 @@ if __name__ == '__main__':
     model_nb   = args.nb
     model_rf   = args.rf
     model_nbp  = args.nbp
-
-    bubbleShock = get_bubbleshock()
-    X = bubbleShock[:,0:-1]
-    y = np.ravel(bubbleShock[:,[-1]])
-
-
-    discretized_y = np.zeros(len(y))
-    for element in range(len(y)):
-        if y[element] != 0:
-            discretized_y[element] = 1
+    model_rfp  = args.rfp
+    use_mpi = 'MPICH_INTERFACE_HOSTNAME' in os.environ
+    k = 10
 
     if model_nb:
         wrapper('naive bayes', k, ['bubbleShock'], verbose=verbose)
     if model_rf:
         wrapper('random forest', k, ['bubbleShock'], verbose=verbose)
     if model_nbp:
-        use_mpi = 'MPICH_INTERFACE_HOSTNAME' in os.environ
-        use_online = False
-
-        if use_mpi and comm.rank == 0:
-            print('will train using MPI')
-        if use_online and comm.rank == 0:
-            print('will train in online mode')
+        wrapper('nbmpi', k, ['bubbleShock'], verbose=verbose, use_mpi=use_mpi)
+    if model_rfp:
+        wrapper('rfmpi', k, ['bubbleShock'], verbose=verbose, use_mpi=use_mpi)
         
-        y = discretized_y
-
-        fp, fn = train_and_test_k_fold(X, y, k=10, verbose=verbose, use_online=use_online, use_mpi=use_mpi)
-
-        if comm.rank == 0:
-            print "PERFORMANCE\t%d\t%d" % (fp, fn)
