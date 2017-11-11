bbf2e093dab2bd2358e91d4024be27cc0671ebf8
saltedsky <a.huang.011@gmail.com>
2017-11-10 16:30:40 -0800

    testing

--- src/learning_wrapper.py
+++ src/learning_wrapper.py
@@ -34,28 +34,28 @@ def wrapper(ML_type, k, data_path, verbose=False, use_online=False, use_mpi=Fals
 
     if ML_type == NAIVE_BAYES:
         if comm.rank == 0:
-            print "############ Training using Naive Bayes ############"
+            print "Training using serial Naive Bayes..."
             y = discretized_y
-            result = nb.train_and_test_k_fold(X, y, k)
+            model = nb.train_and_test_k_fold(X, y, k)
             print
             print
     elif ML_type == RANDOM_FOREST:
         if comm.rank == 0:
-            print "############ Training using Random Forest ############"
-            forest = rf.train_and_test_k_fold(X, y, k)
+            print "Training using serial Random Forest..."
+            forest = rf.train_and_test_k_fold_serial_merge(X, y, k)
             output_feature_importance(forest, data_path)
             print
             print
     elif ML_type == NAIVE_BAYES_MPI:
         if comm.rank == 0:
-            print "############ Training using Parallel Naive Bayes ############"
+            print "Training using parallel Naive Bayes..."
+
+            if use_mpi:
+                print('will train using MPI')
+            if use_online:
+                print('will train in online mode')
 
         y = discretized_y
-
-        if use_mpi and comm.rank == 0:
-            print('will train using MPI')
-        if use_online and comm.rank == 0:
-            print('will train in online mode')
 
         result = train_and_test_k_fold(X, y, nbmpi.train, k=k, verbose=verbose, online=use_online, mpi=use_mpi)
 
@@ -64,21 +64,44 @@ def wrapper(ML_type, k, data_path, verbose=False, use_online=False, use_mpi=Fals
 
     elif ML_type == RANDOM_FOREST_MPI:
         if comm.rank == 0:
-            print "############ Training using Parallel Random Forest ############"
+            print "Training using parallel Random Forest..."
 
-        if use_mpi and comm.rank == 0:
-            print('will train using MPI')
-        if use_online and comm.rank == 0:
-            print('will train in online mode')
+            if use_mpi:
+                print('will train using MPI')
+            if use_online:
+                print('will train in online mode')
 
         result = train_and_test_k_fold(X, y, rfmpi.train, k=k, verbose=verbose, online=use_online, mpi=use_mpi)
         if comm.rank == 0:
             print "PERFORMANCE\t%s" % (result,)
 
+def get_mpi_task_data(X, y, comm = MPI.COMM_WORLD):
+    samps_per_task = X.shape[0] // comm.size
+
+    min_bound = samps_per_task*comm.rank
+    if comm.rank == comm.size - 1:
+        max_bound = X.shape[0]
     else:
-        raise Exception('Machine learning algorithm not recognized')
+        max_bound = min_bound + samps_per_task
+
+    return (X[min_bound:max_bound],
+            y[min_bound:max_bound])
+
+def serial_merge_rf(train_X, test_X, train_y, test_y):
+    samps_per_task = train_X.shape[0] // comm.size
+    trees = []
+    for k in range(1,9):
+        min_bound = samps_per_task*comm.rank
+        if k == 8:
+            max_bound = train_X.shape[0]
+        else:
+            max_bound = min_bound + samps_per_task
+
+        interval = (train_X[min_bound:max_bound], train_y[min_bound:max_bound])
+
 
 if __name__ == '__main__':
+    np.random.seed(0)
 
     # Read command line inputs
     parser = argparse.ArgumentParser(
@@ -97,7 +120,7 @@ if __name__ == '__main__':
     verbose    = args.verbose
     use_online = args.online
     use_mpi = running_in_mpi()
-    k = 10
+    k = 2
 
     for model in args.models:
         wrapper(model, k, args.data_dir, verbose=verbose, use_mpi=use_mpi, use_online=use_online)
